cd290374fd2c511884bea534f2290c2ca4fe8d26
yangzhm <yangzhm@hualu.com>
2018-05-25 11:28:41 +0800

    add sort method

--- chapter 2/selectionsort.py
+++ chapter 2/selectionsort.py
@@ -1,3 +1,5 @@
+import random
+import time
 
 
 def findsmallest(arr):
@@ -12,14 +14,67 @@ def findsmallest(arr):
     return smalles_index
 
 
+def findlargest(arr):
+    largest = arr[0]
+    largest_index = 0
+
+    for i in range(1, len(arr)):
+        if arr[i] > largest:
+            largest_index = i
+            largest = arr[i]
+
+    return largest_index
+
 def selectionsort(arr):
     newArr = []
 
     for i in range(len(arr)):
-        smallest = findsmallest(arr)
+        smallest = findlargest(arr)
         newArr.append(arr.pop(smallest))
 
     return newArr
 
 
+def selectionsort2(arr):
+    for i in range(0, len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[j] > arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
+
+def selectionsort3(arr):
+    n = len(arr)
+
+    for i in range(0, n//2):
+        largest = arr[i]
+        largest_index = i
+        smallest = arr[n-i-1]
+        smallest_index = n-i-1
+        for j in range(i+1, n-i):
+            if arr[j] > largest:
+                largest = arr[j]
+                largest_index = j
+            if arr[j] < smallest:
+                smallest = arr[j]
+                smallest_index = j
+        arr[i], arr[largest_index] = arr[largest_index], arr[i]
+        arr[n-i-1], arr[smallest_index] = arr[smallest_index], arr[n-i-1]
+
+    return arr
+
+
+nums1 = []
+nums2 = []
+for i in range(10000):
+  num = random.randint(1, 100000)
+  nums1.append(num)
+  nums2.append(num)
+
 print(selectionsort([5, 3, 6, 2, 2, 10]))
+
+print("selection sort 2", time.time())
+selectionsort2(nums1)
+print('selection sort 3', time.time())
+selectionsort3(nums2)
+print('end', time.time())
