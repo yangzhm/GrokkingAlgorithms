9545588ca62efede2f9ba044f1ba22b04963453f
yangzhm <yangzhm@hualu.com>
2018-05-28 16:18:53 +0800

    add chapter 4

---
+++ chapter 4/quicksort.py
@@ -0,0 +1,13 @@
+
+
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+
+print(quick_sort([1, 4, 20, 3, 9, 89]))
